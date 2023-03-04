dc227170469a26de86f90b3bfcb64b57f148fe35
Nahin27 <nahin271199@gmail.com>
2023-03-04 09:59:47 +0600

    Set up an index.eta

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -2,11 +2,22 @@ import { serve } from "./deps.js";
 import { sql } from "./database/database.js";
 import { renderFile, configure } from "https://deno.land/x/eta@v1.11.0/mod.ts";
 
-await sql`insert into shopping_lists (name) values ('nahin')`;
+// Configuring the file to find the views files 
+configure({
+  views: "./views",
+});
+
+// response detail to parse the .eta file as html
+const responseDetails = {
+  headers: {
+    "Content-Type": "text/html;charset=UTF-8",
+  },
+};
 
 const handleRequest = async (request) => {
+  const data = {};
   console.log("Responding with Hello world!");
-  return new Response("Hello world!");
+  return new Response(await renderFile("index.eta", data), responseDetails);
 };
 
 serve(handleRequest, { port: 7777 });
